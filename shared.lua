c60918c8571a220bc53e35e49b59a28bb7982c18
moon91210 <smulders@protonmail.com>
2019-06-06 22:43:36 +0200

    add sendTo argument to server side

--- shared.lua
+++ shared.lua
@@ -1,11 +1,15 @@
 local exportedFunctions = {'info', 'warning', 'error', 'success'}
 
 for i=1, #exportedFunctions do
-	_G[exportedFunctions[i]] = function(msg, options)
+	_G[exportedFunctions[i]] = function(msg, options, target)
 		assert(type(msg) == 'string', '(popup) argument 1 not a string')
 
 		if options then
 			assert(type(options) == 'table', '(popup) argument 2 not a table')
+		end
+
+		if target then
+			assert(type(target) == 'userdata', '(popup) argument 3 not an element')
 		end
 
 		local opts = options or {}
@@ -15,7 +19,7 @@ for i=1, #exportedFunctions do
 		if localPlayer then -- check if this is the client side
 			createNotification(msg, opts)
 		else
-			triggerClientEvent('onNotify', resourceRoot, msg, opts)
+			triggerClientEvent(target or root, 'onNotify', resourceRoot, msg, opts)
 		end
 	end
 end
